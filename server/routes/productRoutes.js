4de2e94f0ccd702f3031578b3b4f413359805120
Your Name <you@example.com>
2024-05-30 10:41:42 +0100

    best sales

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,24 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controller/productController");
 const jwtMiddleware=require('../middleware/jwtMiddleware')
-// // post a product (protected because its a seller feature)
-// router.post("/create",jwtMiddleware, productController.createProduct);
-// // get a product by the sales numbers
-// router.get('/sales',productController.getBestSellers)
-
-// // get all products
-// router.get("/get", productController.getAllProducts);
-// // get one product
-// router.get("/:id", productController.getProductById);
-
-// // update a product (protected because its a seller feature)
-// router.put("/:id",jwtMiddleware, productController.updateProduct);
-
-// // delete a produt (protected because its a seller feature)
-// router.delete("/:id",jwtMiddleware, productController.deleteProduct);
-
 // post a product (protected because its a seller feature)
-router.post("/create", productController.createProduct);
+router.post("/create",jwtMiddleware, productController.createProduct);
 // get a product by the sales numbers
 router.get('/sales',productController.getBestSellers)
 
@@ -29,9 +13,10 @@ router.get("/get", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 
 // update a product (protected because its a seller feature)
-router.put("/:id", productController.updateProduct);
+router.put("/:id",jwtMiddleware, productController.updateProduct);
 
 // delete a produt (protected because its a seller feature)
-router.delete("/:id", productController.deleteProduct);
+router.delete("/:id",jwtMiddleware, productController.deleteProduct);
+
 
 module.exports = router;
